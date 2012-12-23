28605d7ea85186307c6f128d08af718ec47670ec
Rico Sta. Cruz <rico@ambiescent.com>
2012-12-23 23:51:40 +0800

    Tests: use assert_equal.

--- test/tps_test.rb
+++ test/tps_test.rb
@@ -8,48 +8,48 @@ class MyTest < UnitTest
 
   test "Has tasks" do
     assert @list.tasks?
-    assert @list.tasks.size == 2
+    assert_equal 2, @list.tasks.size
     assert @list.tasks.first.tasks.size >= 2
   end
 
   test "Explicit percent" do
     task = @milestone.tasks[0]
     assert task.in_progress?
-    assert task.status == :in_progress
-    assert task.percent == 0.25
+    assert_equal :in_progress, task.status
+    assert_equal 0.25, task.percent
   end
 
   test "Overriding percent" do
     task = @milestone.tasks[2]
-    assert task.name == "Overridden percent"
+    assert_equal "Overridden percent", task.name
     assert task.in_progress?
-    assert task.status == :in_progress
-    assert task.percent == 0.5
+    assert_equal :in_progress, task.status
+    assert_equal 0.5, task.percent
   end
 
   test "Points" do
     task = @milestone.tasks[1]
-    assert task.points == 2.0
+    assert_equal 2.0, task.points
   end
 
   test "Explicit points" do
     task = @milestone.tasks[3]
-    assert task.points == 15
-    assert task.percent == 0.75
-    assert task.points_done == 11.25
+    assert_equal 15, task.points
+    assert_equal 0.75, task.percent
+    assert_equal 11.25, task.points_done
   end
 
   test "Compound points" do
     task = @milestone.tasks[4]
-    assert task.points == 6
-    assert task.percent == 0.50
+    assert_equal 6, task.points
+    assert_equal 0.50, task.percent
   end
 
   test "Point rescaling" do
     task = @milestone.tasks[5]
-    assert task.points == 8
-    assert task.points_done == 4.0
-    assert task.percent == 0.5
+    assert_equal 8, task.points
+    assert_equal 4.0, task.points_done
+    assert_equal 0.5, task.percent
   end
 
   test "In progress" do
@@ -76,23 +76,25 @@ class MyTest < UnitTest
   end
 
   test "Lookup" do
-    assert @list['Milestone 1'] == @milestone
+    assert_equal @milestone, @list['Milestone 1']
   end
 
   test "Task#breadcrumbs" do
     crumbs = @list['Milestone 1']['User login']['Signup'].breadcrumbs
-    assert crumbs == [
+    expected = [
       @list['Milestone 1'],
       @list['Milestone 1']['User login'],
       @list['Milestone 1']['User login']['Signup']
     ]
+    assert_equal expected, crumbs
   end
 
   test "Task#breadcrumbs(false)" do
     crumbs = @list['Milestone 1']['User login']['Signup'].breadcrumbs(false)
-    assert crumbs == [
+    expected = [
       @list['Milestone 1'],
       @list['Milestone 1']['User login']
     ]
+    assert_equal expected, crumbs
   end
 end
