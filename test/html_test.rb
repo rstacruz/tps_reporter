40ceff008324c16edb4a7fde2265286668dcd274
Rico Sta. Cruz <rico@ambiescent.com>
2012-12-24 00:35:27 +0800

    Tests: add HTML generator test.

---
+++ test/html_test.rb
@@ -0,0 +1,19 @@
+require File.expand_path('../test_helper', __FILE__)
+
+class HtmlTest < UnitTest
+  TESTS = %w[hello.yml multi_sprints.yml]
+
+  TESTS.each do |yml_file|
+    context yml_file do
+      setup do
+        @list = TPS::TaskList.new yaml: f(yml_file)
+      end
+
+      test "HTML" do
+        html = @list.to_html
+        assert html.include?("span")
+        assert html.include?("body")
+      end
+    end
+  end
+end
