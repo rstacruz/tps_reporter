29325f7e27a818d319b1290aca786a780a33b8b8
Rico Sta. Cruz <rico@ambiescent.com>
2012-12-23 23:58:03 +0800

    Tests: add more tests for sprint points fix.

--- test/sprint_test.rb
+++ test/sprint_test.rb
@@ -66,17 +66,27 @@ class SprintTest < UnitTest
     setup do
       @list = TPS::TaskList.new yaml: f('sprint_points.yml')
       @milestone = @list.tasks.first
-      @s1 = @list.sprints['s1']
-      @s2 = @list.sprints['s2']
+      @s = %w[_ s1 s2 s3 s4 s5].map { |id| @list.sprints[id] }
     end
 
     test "S1 points" do
-      puts "\n", @s1.sublist.to_markdown
-      assert_equal 1.0, @s1.points
+      assert_equal 1.0, @s[1].points
     end
 
     test "S2 points" do
-      assert_equal 3.0, @s2.points
+      assert_equal 3.0, @s[2].points
+    end
+
+    test "S3 points" do
+      assert_equal 0.0, @s[3].points
+    end
+
+    test "S4 points" do
+      assert_equal 2.5, @s[4].points
+    end
+
+    test "S5 points" do
+      assert_equal 1.0, @s[5].points
     end
   end
 end
